f2becb61c95d601eb76a462b4e6a8e3f6a5622d9
gun4ansky <gun_project@mail.ru>
2023-01-08 14:21:47 +0500

    исправление для задания 3.3.2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,18 @@
-﻿const string MyName = "Илья";
-Console.WriteLine(MyName);
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Илья";
+            byte MyAge = 25;
+            bool MyPet = true;
+            double MyShoeSize = 43.5;
+
+            Console.WriteLine("Меня зовут "+ MyName);
+            Console.WriteLine("мне " + MyAge);
+            Console.WriteLine("Есть ли у меня живность в доме? " + MyPet);
+            Console.WriteLine("Мой размер ноги " + MyShoeSize);
+        }
+    }
+}
