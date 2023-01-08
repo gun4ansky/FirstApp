2d8d96016759c5c2519fa47c4687c0f19bb5290f
gun4ansky <gun_project@mail.ru>
2023-01-08 14:30:30 +0500

    а

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@
             double MyShoeSize = 43.5;
 
             Console.WriteLine("Меня зовут "+ MyName);
-            Console.WriteLine("мне " + MyAge);
+            Console.WriteLine("Мне " + MyAge);
             Console.WriteLine("Есть ли у меня живность в доме? " + MyPet);
             Console.WriteLine("Мой размер ноги " + MyShoeSize);
         }
